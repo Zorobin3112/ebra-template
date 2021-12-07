e6fb1dd17d41204a5f3f96758a380af364f0d4d5
Zorobin3112 <khanhnguyenvan3112@gmail.com>
2021-12-07 17:58:14 +0900

    first

---
+++ config-overrides.js
@@ -0,0 +1,27 @@
+const { override, addBabelPlugins } = require('customize-cra');
+
+module.exports = override(
+  ...addBabelPlugins([
+    "babel-plugin-root-import",
+    {
+        "paths": [
+            {
+                "rootPathSuffix": "src/view/components",
+                "rootPathPrefix": "$components/"
+            },
+            {
+                "rootPathSuffix": "./src/view/assets",
+                "rootPathPrefix": "$assets/"
+            },
+            {
+                "rootPathSuffix": "./src/my-redux",
+                "rootPathPrefix": "$my-redux/"
+            },
+            {
+                "rootPathSuffix": "./src/utils",
+                "rootPathPrefix": "$utils/"
+            }
+        ]
+    },
+  ])
+);
